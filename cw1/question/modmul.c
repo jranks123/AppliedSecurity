976e1cec17eed26e963695064b91f18cc1d9b5a2
JE Rankin <jr0436@seoul.cs.bris.ac.uk>
2014-02-21 17:05:48 +0000

    Montgomery Reduction done

--- cw1/question/modmul.c
+++ cw1/question/modmul.c
@@ -77,6 +77,7 @@ void newPow(mpz_t t, mpz_t x, mpz_t y, mpz_t N){
   unsigned long long nModB = mpz_getlimbn(omega, 0);
  // mpz_mod(x, x, N);
   ZnModMul(xBar, x, pSqared, N, omega, u2, temp, b, r, t2, nModB);
+  gmp_printf("T = %Zd\n", xBar);
   ZnModMul(t, one, pSqared , N, omega, u2, temp, b, r, t2, nModB);
   calculateT(tSize, T, xBar, N, omega, u2, temp, b, r, t2, nModB);
   int i = mpz_sizeinbase (y, 2) -1;
@@ -240,34 +241,48 @@ void ZnModMul(mpz_t result, mpz_t x, mpz_t y, mpz_t N, mpz_t omega, mpz_t u, mpz
 
 }*/
 
-/*void montRed(mpz_t x, mpz_t omega, mpz_t N){
-    mpz_t r, b, u;
-    mpz_inits(r, b, u, NULL);
+void montRed(mpz_t result, mpz_t x, mpz_t omega, mpz_t N){
+    mpz_t r, b, u, Nshift;
+    mpz_inits(r, b, Nshift, u, NULL);
     mpz_setbit(b, 64);
-    mpz_set(r, t);
+    mpz_set(r, x);
     int i;
     size_t nx = mpz_size( N ); 
     unsigned long long x0 = mpz_getlimbn(x,0);
-    unsigned long long rI;
-    for( i = 0; i < Nx; i++){
-
+    unsigned long long omega0 = mpz_getlimbn(omega,0);
+    unsigned long long uInt;
+    for( i = 0; i < nx; i++){
+      uInt = mpz_getlimbn(r, i)*omega0; 
+      mpz_mul_2exp(Nshift, N, 64*i); 
+      mpz_mul_ui(u, Nshift, uInt); 
+      mpz_add(r, r, u); 
     }
+    mpz_fdiv_q_2exp(r, r, 64*nx); 
+      if(mpz_cmp(r, N)>=0){
+      mpz_sub(r, r, N);
 
-}*/
+   }
+   mpz_set(result, r);
+}
+
 
 
 void test() {
   // fill in this function with solution
-    mpz_t omega, N, e, m, c;
+    mpz_t omega, N, e, m, c, result;
     int lineCount = 0;
-    mpz_inits(N, omega, e,m,c,NULL);
+    mpz_inits(N, omega, e,m,c, result, NULL);
 
     do{
       lineCount = gmp_scanf( "%Zx\n%Zx\n%Zx", N, e, m );
        if(lineCount == 3){
+        mpz_set_ui(m, 421);
+        mpz_set_ui(N, 667);
         calculateOmega(omega, N);
-       // montRed(m, omega, N);
-        gmp_printf("T = %Zd\n", c);
+        montRed(result, m, omega, N);
+        gmp_printf("T = %Zd\n", result);
+        newPow(result, m, m, N);
+
        // gmp_printf("%Zx", c);
        }
     } while (lineCount == 3 );
