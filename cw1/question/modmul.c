557cf1cace4dcebe091fc72f2797014004a92460
JE Rankin <jr0436@abuja.cs.bris.ac.uk>
2014-02-21 17:58:55 +0000

    Finalised code

--- cw1/question/modmul.c
+++ cw1/question/modmul.c
@@ -14,33 +14,8 @@
        __typeof__ (b) _b = (b); \
      _a > _b ? _a : _b; })
 
-/*
-Perform stage 1:
-
-- read each 3-tuple of N, e and m from stdin,
-- compute the RSA encryption c,
-- then write the ciphertext c to stdout.
-*/
 
 
-/*void mAryFixedRecode(mpz_t y, int k){
-    size_t nx = mpz_size( y );
-    gmp_printf("Y = %Zx\n", y);
-    gmp_printf("size - %d\n", nx);
-    int i;
-    mp_limb_t tx[ nx ];
-    mpz_export( tx, NULL, -1, sizeof( mp_limb_t ), -1, 0, y);
-    for (i = 0; i<nx; i++){
-        gmp_printf( "%d -  %llu\n",i, tx[i] );
-    }
-}
-
-
-void mAryFixed(mpz_t result, mpz_t x, mpz_t y, mpz_t N){
-    int k = 2;
-    mAryFixedRecode(y, k);
-
-}*/
 
     //mpz_tstbit
 void CRT(mpz_t N,mpz_t d,mpz_t p,mpz_t q,mpz_t d_p,mpz_t d_q,mpz_t i_p,mpz_t i_q,mpz_t c,mpz_t m,mpz_t u,mpz_t temp1,mpz_t temp2){
@@ -62,22 +37,20 @@ void CRT(mpz_t N,mpz_t d,mpz_t p,mpz_t q,mpz_t d_p,mpz_t d_q,mpz_t i_p,mpz_t i_q
     mpz_add(m, m, temp2);
     mpz_mod(m,m,N);
 
+    mpz_clear(cHold);
+
 }
 
-void calculateT(int tSize, mpz_t* T, mpz_t x, mpz_t N, mpz_t omega, mpz_t u2, mpz_t temp, mpz_t b, mpz_t r, unsigned long long t, unsigned long long nModB){
+void calculateT(int tSize, mpz_t* T, mpz_t x, mpz_t N, mpz_t omega, mpz_t u2, mpz_t temp, mpz_t b, mpz_t r, unsigned long long t, unsigned long long nModB, size_t Tx){
     int i = 0;
     int k = 0;
     mpz_t xSquared;
     mpz_inits(T[0], xSquared, NULL);
     mpz_set(T[0], x);
-    ZnModMul(xSquared, x, x, N, omega, u2, temp, b, r, t, nModB); 
-    //mpz_mul(xSquared, x, x);
-    //mpz_mod(xSquared, xSquared, N);  
+    ZnModMul(xSquared, x, x, N, omega, u2, temp, b, r, t, nModB, Tx); 
     for(i = 1; i < tSize; i++){
       mpz_init(T[i]);
-      ZnModMul(T[i], T[i-1], xSquared, N, omega, u2, temp, b, r, t, nModB);
-      //mpz_mul(T[i], T[i-1], xSquared);
-      //mpz_mod(T[i], T[i], N);  
+      ZnModMul(T[i], T[i-1], xSquared, N, omega, u2, temp, b, r, t, nModB, Tx);
     }
     mpz_clear(xSquared);
 }
@@ -93,12 +66,13 @@ void newPow(mpz_t t, mpz_t x, mpz_t y, mpz_t N){
   mpz_set_ui(pSqared, 1);
   mpz_set_ui(one, 1);
   calculatePSquared(pSqared, N);
-  calculateOmega(omega,N);
+  calculateOmega(omega,N, b);
   unsigned long long nModB = mpz_getlimbn(omega, 0);
+  size_t Tx = mpz_size( N );
  // mpz_mod(x, x, N);
-  ZnModMul(xBar, x, pSqared, N, omega, u2, temp, b, r, t2, nModB);
-  ZnModMul(t, one, pSqared , N, omega, u2, temp, b, r, t2, nModB);
-  calculateT(tSize, T, xBar, N, omega, u2, temp, b, r, t2, nModB);
+  ZnModMul(xBar, x, pSqared, N, omega, u2, temp, b, r, t2, nModB, Tx);
+  ZnModMul(t, one, pSqared , N, omega, u2, temp, b, r, t2, nModB, Tx);
+  calculateT(tSize, T, xBar, N, omega, u2, temp, b, r, t2, nModB, Tx);
   int i = mpz_sizeinbase (y, 2) -1;
   size_t l, u;
   int e, c, g;
@@ -121,13 +95,13 @@ void newPow(mpz_t t, mpz_t x, mpz_t y, mpz_t N){
         }
       }
       for(g = 0; g < i-l+1; g++){
-         ZnModMul(t, t, t, N, omega, u2, temp, b, r, t2, nModB);
+         ZnModMul(t, t, t, N, omega, u2, temp, b, r, t2, nModB, Tx);
       }
 
         if(u != 0){
             size_t q = (u-1)/2;
 
-            ZnModMul(t, t, T[q], N, omega, u2, temp, b, r, t2, nModB);
+            ZnModMul(t, t, T[q], N, omega, u2, temp, b, r, t2, nModB, Tx);
         }
       i = l-1;
     }
@@ -135,17 +109,21 @@ void newPow(mpz_t t, mpz_t x, mpz_t y, mpz_t N){
    for(i = 0; i < tSize; i++){
       mpz_clear( T[i] );
   }
-  ZnModMul(t, t, one, N, omega, u2, temp, b, r, t, nModB);
+  ZnModMul(t, t, one, N, omega, u2, temp, b, r, t, nModB, Tx);
    mpz_clear(xBar);
    mpz_clear(omega);
    mpz_clear(pSqared);
    mpz_clear(one);
+   mpz_clear(u2);
+   mpz_clear(temp);
+   mpz_clear(b);
+   mpz_clear(r);
 }
 
-void calculateOmega(mpz_t x, mpz_t N){
+void calculateOmega(mpz_t x, mpz_t N, mpz_t b){
 //  gmp_printf("%Zd\n", N);
-    mpz_t zero, b;
-    mpz_inits(zero, b, NULL);
+    mpz_t zero;
+    mpz_init(zero);
     int w = 64;
     mpz_set_ui(b,0);
     mpz_setbit(b, 64);
@@ -160,32 +138,9 @@ void calculateOmega(mpz_t x, mpz_t N){
     }
     mpz_set_ui (x,t);
     mpz_sub(x, b, x);
+    mpz_clear(zero );
 }
 
-/*void calculateOmega2(mpz_t x, mpz_t N){
-//  gmp_printf("%Zd\n", N);
-    mpz_set_ui(x, 1);
-    mpz_t b, temp;
-    mpz_t zero;
-    mpz_init(zero);
-    mpz_inits(b, temp, NULL);
-    mpz_set_ui(b,0);
-    mpz_setbit(b, 64);
-    //gmp_printf("%Zd\n", b);
-    int i;
-    for(i = 0; i < 64; i++){
-      //gmp_printf("%Zd\n", x);
-      mpz_mul(x, x, x);
-      mpz_mod(temp, N, b);
-     // gmp_printf("temp = %Zd\n", temp);
-      mpz_mul(x, x, temp);
-     // gmp_printf("x after mul= %Zd\n", x);
-      mpz_mod(x, x, b);
-      //gmp_printf("x after mod = %Zd\n", x);
-    }
-    mpz_sub(x, zero, x);
-    mpz_mod(x, x, b);
-}*/
 
 
 
@@ -203,19 +158,17 @@ void calculateP(mpz_t t, mpz_t N){
     mpz_setbit(t, mpz_size( N )*64);
 }
 
-void ZnModMul(mpz_t result, mpz_t x, mpz_t y, mpz_t N, mpz_t omega, mpz_t u, mpz_t temp, mpz_t b, mpz_t r, unsigned long long t, unsigned long long nModB){
+
+
+void ZnModMul(mpz_t result, mpz_t x, mpz_t y, mpz_t N, mpz_t omega, mpz_t u, mpz_t temp, mpz_t b, mpz_t r, unsigned long long t, unsigned long long nModB, size_t Tx){
   int i;
-  size_t Tx = mpz_size( N );
   mpz_set_ui(r, 0);
   unsigned long long x0;
- // x0  = mpz_getlimbn(x,0);
   x0  = x->_mp_d[0];
   unsigned long long yI;
   size_t ySize = mpz_size(y);
   for(i = 0; i < Tx; i++){
-   // yI = mpz_getlimbn(y,i);
     yI = i<ySize? y->_mp_d[i]: 0;
-   // printf("%d\n", yI);
     t = (r->_mp_d[0]+(yI*x0))*nModB;
     mpz_mul_ui(temp, x,  yI);
     mpz_mul_ui(u, N, t);
@@ -234,57 +187,14 @@ void ZnModMul(mpz_t result, mpz_t x, mpz_t y, mpz_t N, mpz_t omega, mpz_t u, mpz
 }
 
 
+/*
+Perform stage 1:
 
-void montRed(mpz_t result, mpz_t x, mpz_t omega, mpz_t N){
-    mpz_t r, b, u, Nshift;
-    mpz_inits(r, b, Nshift, u, NULL);
-    mpz_setbit(b, 64);
-    mpz_set(r, x);
-    int i;
-    size_t nx = mpz_size( N ); 
-    unsigned long long x0 = mpz_getlimbn(x,0);
-    unsigned long long omega0 = mpz_getlimbn(omega,0);
-    unsigned long long uInt;
-    for( i = 0; i < nx; i++){
-      uInt = mpz_getlimbn(r, i)*omega0; 
-      mpz_mul_2exp(Nshift, N, 64*i); 
-      mpz_mul_ui(u, Nshift, uInt); 
-      mpz_add(r, r, u); 
-    }
-    mpz_fdiv_q_2exp(r, r, 64*nx); 
-      if(mpz_cmp(r, N)>=0){
-      mpz_sub(r, r, N);
+- read each 3-tuple of N, e and m from stdin,
+- compute the RSA encryption c,
+- then write the ciphertext c to stdout.
+*/
 
-   }
-   mpz_set(result, r);
-}
-
-
-
-void test() {
-  // fill in this function with solution
-    mpz_t omega, N, e, m, c, result;
-    int lineCount = 0;
-    mpz_inits(N, omega, e,m,c, result, NULL);
-
-    do{
-      lineCount = gmp_scanf( "%Zx\n%Zx\n%Zx", N, e, m );
-       if(lineCount == 3){
-        mpz_set_ui(m, 421);
-        mpz_set_ui(N, 667);
-        calculateOmega(omega, N);
-        montRed(result, m, omega, N);
-        gmp_printf("T = %Zd\n", result);
-        newPow(result, m, m, N);
-
-       // gmp_printf("%Zx", c);
-       }
-    } while (lineCount == 3 );
-    mpz_clear( N );
-    mpz_clear( e );
-    mpz_clear( m );
-
-}
 
 void stage1() {
   // fill in this function with solution
@@ -353,30 +263,20 @@ void stage3() {
     fp = fopen("/dev/urandom", "r");
     int output = fread(&randVal, 8, 1, fp);
     fclose(fp);
-
     if(!output)
     {
           printf("Error reading values");
           return;
     }
-
-
-
-
-
     gmp_randstate_t randState;
-
     mpz_inits(p,q,g,h,m,c1,c2,k, seed, NULL);
-
-
     mpz_set_ui(seed, randVal);
     gmp_randinit_default(randState);
     do{
-
       lineCount  =gmp_scanf( "%Zx\n%Zx\n%Zx\n%Zx\n%Zx",  p,q,g,h,m );
       gmp_randseed (randState, seed);
       mpz_urandomm(k, randState, q);
-      mpz_set_ui(k, 1);
+    //  mpz_set_ui(k, 1);
       if(lineCount == 5){
         newPow(c1, g, k, p);
         newPow(c2, h, k, p);
--- cw1/question/modmul.h
+++ cw1/question/modmul.h
@@ -11,3 +11,51 @@
 
 #endif
 
+void montRed(mpz_t result, mpz_t x, mpz_t omega, mpz_t N, mpz_t b){
+    mpz_t r, u, Nshift;
+    mpz_inits(r, Nshift, u, NULL);
+    mpz_set(r, x);
+    int i;
+    size_t nx = mpz_size( N ); 
+    unsigned long long x0 = mpz_getlimbn(x,0);
+    unsigned long long omega0 = mpz_getlimbn(omega,0);
+    unsigned long long uInt;
+    for( i = 0; i < nx; i++){
+      uInt = mpz_getlimbn(r, i)*omega0; 
+      mpz_mul_2exp(Nshift, N, 64*i); 
+      mpz_mul_ui(u, Nshift, uInt); 
+      mpz_add(r, r, u); 
+    }
+    mpz_fdiv_q_2exp(r, r, 64*nx); 
+      if(mpz_cmp(r, N)>=0){
+      mpz_sub(r, r, N);
+
+   }
+   mpz_set(result, r);
+}
+
+
+void test() {
+  // fill in this function with solution
+    mpz_t omega, N, e, m, b, c, result;
+    int lineCount = 0;
+    mpz_inits(N, omega, e,m,c, b, result, NULL);
+    mpz_setbit(b,64);
+    do{
+      lineCount = gmp_scanf( "%Zx\n%Zx\n%Zx", N, e, m );
+       if(lineCount == 3){
+        mpz_set_ui(m, 421);
+        mpz_set_ui(N, 667);
+        calculateOmega(omega, N, b);
+        montRed(result, m, omega, N, b);
+        gmp_printf("T = %Zd\n", result);
+        newPow(result, m, m, N);
+
+       // gmp_printf("%Zx", c);
+       }
+    } while (lineCount == 3 );
+    mpz_clear( N );
+    mpz_clear( e );
+    mpz_clear( m );
+
+}
